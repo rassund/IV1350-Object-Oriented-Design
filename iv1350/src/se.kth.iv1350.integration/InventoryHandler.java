071c6d10939f8c95abe9bfa8dfc5c1e9eadb196f
Rasmus Sundbom <rasmus@driftgruppen.se>
2025-04-22 16:32:05 +0200

    Fix merge

--- iv1350/src/se.kth.iv1350.integration/InventoryHandler.java
+++ iv1350/src/se.kth.iv1350.integration/InventoryHandler.java
@@ -1,7 +1,27 @@
 package se.kth.iv1350.integration;
 
+import se.kth.iv1350.DTO.ItemDTO;
+import se.kth.iv1350.model.Amount;
+import se.kth.iv1350.model.VAT;
+
 /**
  * Used for handling the database where all items in the store's inventory is written down.
  */
 public class InventoryHandler {
+    ItemDTO[] dummyItems = new ItemDTO[3];
+
+    public InventoryHandler() {
+        for (int i = 0; i < dummyItems.length; i++) {
+            dummyItems[i] = new ItemDTO(new Amount(40+i), VAT.LOW, "Dummy item nr " + i, i);
+        }
+    }
+
+    public ItemDTO fetchInfo(int itemID) {
+        for (ItemDTO item : dummyItems) {
+            if (item.getID() == itemID) {
+                return item;
+            }
+        }
+        return null;
+    }
 }
