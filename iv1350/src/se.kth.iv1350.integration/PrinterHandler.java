00f8d4cf95e68ff07baf4736e5d3c378030675bb
Mateusz Kaszyk <mekmmeekk@proton.me>
2025-04-26 23:57:34 +0200

    Made the receipt have the relevant information directly.

--- iv1350/src/se.kth.iv1350.integration/PrinterHandler.java
+++ iv1350/src/se.kth.iv1350.integration/PrinterHandler.java
@@ -1,13 +1,6 @@
 package se.kth.iv1350.integration;
 
-import se.kth.iv1350.DTO.ItemDTO;
-import se.kth.iv1350.DTO.SaleDTO;
 import se.kth.iv1350.model.Receipt;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.ArrayList;
 
 /**
  * Used for handling the software for the printer (which prints the receipt).
@@ -16,33 +9,25 @@ public class PrinterHandler {
     /**
      * Sends all text to print on the receipt to the printer software, and starts the printing.
      * (Currently, the printing software does not exist. This method therefore now prints a dummy version of the receipt onto the console.)
-     * @param Receipt Contains all info to be printed onto the receipt.
+     * @param receipt Contains all info to be printed onto the receipt.
      */
     public void printReceipt(Receipt receipt) {
         fakePrintReceipt(receipt);
     }
 
     private void fakePrintReceipt(Receipt receipt) {
-        SaleDTO saleDTO = receipt.getSaleDTO();
         System.out.println("------------------ Begin receipt -------------------");
-        System.out.println("Time of Sale: " + getDate(saleDTO.getDateTime()) + " " + getTime(saleDTO.getDateTime()));
+        System.out.println("Time of Sale: " + receipt.getDateOfSale() + " " + receipt.getTimeOfSale());
         System.out.println();
         System.out.println();
         System.out.println();
-        System.out.println("Total: " + saleDTO.getTotalPrice());
-        System.out.println("VAT: " + saleDTO.getTotalVAT());
+        System.out.println("Total: " + receipt.getTotalPrice());
+        System.out.println("VAT: " + receipt.getTotalVAT());
         System.out.println();
-        System.out.println("Cash: " + saleDTO.getAmountPaid());
-        System.out.println("Change: " + saleDTO.getChange());
+        System.out.println("Cash: " + receipt.getAmountPaid());
+        System.out.println("Change: " + receipt.getChange());
         System.out.println("------------------ End receipt -------------------");
         System.out.println();
     }
 
-    private LocalDate getDate(LocalDateTime dateTime) {
-        return dateTime.toLocalDate();
-    }
-
-    private LocalTime getTime(LocalDateTime dateTime) {
-        return dateTime.toLocalTime().withSecond(0).withNano(0);
-    }
 }
--- iv1350/src/se.kth.iv1350.model/Receipt.java
+++ iv1350/src/se.kth.iv1350.model/Receipt.java
@@ -2,10 +2,39 @@ package se.kth.iv1350.model;
 
 import se.kth.iv1350.DTO.SaleDTO;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Receipt {
-    SaleDTO saleDTO;
+    private final LocalDate dateOfSale;
+    private final  LocalTime timeOfSale;
+    private final  Amount totalPrice;
+    private final  Amount totalVAT;
+    private final  Amount amountPaid;
+    private final  Amount change;
 
-    public Receipt(SaleDTO saleDTO) { this.saleDTO = saleDTO; }
 
-    public SaleDTO getSaleDTO() { return saleDTO; }
+
+    public Receipt(SaleDTO saleDTO) {
+        this.dateOfSale = saleDTO.getDateTime().toLocalDate();
+        this.timeOfSale = saleDTO.getDateTime().toLocalTime().withSecond(0).withNano(0);
+        this.totalPrice = saleDTO.getTotalPrice();
+        this.totalVAT = saleDTO.getTotalVAT();
+        this.amountPaid = saleDTO.getAmountPaid();
+        this.change = saleDTO.getChange();
+
+    }
+
+
+    public LocalDate getDateOfSale() { return dateOfSale; }
+
+    public LocalTime getTimeOfSale() { return timeOfSale; }
+
+    public Amount getTotalPrice() { return totalPrice; }
+
+    public Amount getTotalVAT() { return totalVAT; }
+
+    public Amount getAmountPaid() { return amountPaid; }
+
+    public Amount getChange() { return change; }
 }
