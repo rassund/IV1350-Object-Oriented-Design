53c77ed6c5c52252b9d76d655deef974d1617811
Mateusz Kaszyk <mekmmeekk@proton.me>
2025-04-28 22:35:23 +0200

    Update Receipt.java

--- iv1350/src/se.kth.iv1350.model/Receipt.java
+++ iv1350/src/se.kth.iv1350.model/Receipt.java
@@ -20,10 +20,9 @@ public final class Receipt {
     private final Amount change;
 
     /**
-     * Creates a <code>receipt</code> for a finished instance of a sale.
-     * @param saleDTO contains all the needed information for the <code>receipt</code>.
+     * Creates a {@link Receipt} object for a finished instance of a sale.
+     * @param saleDTO a {@link SaleDTO} object representing the sale that the <code>receipt</code> is a proof of.
      */
-
     public Receipt(SaleDTO saleDTO) {
         this.dateTimeOfSale = saleDTO.dateTime();
         this.totalPrice = saleDTO.totalPrice();
@@ -35,43 +34,43 @@ public final class Receipt {
 
     /**
      * Gets the date for a sale.
-     * @return A <code>LocalDate</code> representing the date in local time for the receipts purchase.
+     * @return A {@link LocalDate} representing the date in local time for the receipts purchase.
      */
     public LocalDate getDateOfSale() { return dateTimeOfSale.toLocalDate(); }
 
     /**
      * Gets the hour and minute (but not seconds or anything below) for a purchase.
-     * @return A <code>LocalDate</code> showing the hour and minute in local time for the receipts purchase.
+     * @return A {@link LocalTime} showing the hour and minute in local time for the receipts purchase.
      */
     public LocalTime getTimeOfSale() { return dateTimeOfSale.toLocalTime().withSecond(0).withNano(0); }
 
     /**
      * Gets the total price.
-     * @return An <code>Amount</code> representing the total price (Including VAT) for all items in the sale.
+     * @return An {@link Amount} representing the total price (Including VAT) for all items in the sale.
      */
     public Amount getTotalPrice() { return totalPrice; }
 
     /**
      * Gets the total VAT.
-     * @return An <code>Amount</code> representing total VAT for all items in the sale.
+     * @return An {@link Amount} representing total VAT for all items in the sale.
      */
     public Amount getTotalVAT() { return totalVAT; }
 
     /**
      * Gets a list of all items in a sale.
-     * @return A <code>ArrayList<ItemInBasketDTO></code> representing all the items bought in the sale.
+     * @return A {@link ArrayList} containing {@link ItemInBasketDTO} objects representing all the items bought in the sale.
      */
     public ArrayList<ItemInBasketDTO> getItemsBought() { return itemsBought; }
 
     /**
      * Gets the amount a customer has paid in a sale.
-     * @return A <code>Amount</code> representing what the customer paid in a sale, this should be higher than or the same as the totalPrice.
+     * @return A {@link Amount} representing what the customer paid in a sale, this should be higher than or the same as the totalPrice.
      */
     public Amount getAmountPaid() { return amountPaid; }
 
     /**
      * Gets the change to give back to a customer in a sale.
-     * @return An <code>Amount> representing the change to give back in a sale, It's the difference between amountPaid and totalPrice.
+     * @return An {@link Amount} representing the change to give back in a sale, It's the difference between amountPaid and totalPrice.
      */
     public Amount getChange() { return change; }
 }
