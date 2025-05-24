72647dbe0e2dda9eb5fe7b792ecd6ddb6b0b56bc
Mateusz Kaszyk <mekmmeekk@proton.me>
2025-05-24 22:45:54 +0200

    Update PrinterHandlerTest.java

--- iv1350/test/se/kth/iv1350/integration/PrinterHandlerTest.java
+++ iv1350/test/se/kth/iv1350/integration/PrinterHandlerTest.java
@@ -5,14 +5,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import se.kth.iv1350.DTO.ItemInBasketDTO;
 import se.kth.iv1350.DTO.SaleDTO;
+import se.kth.iv1350.controller.Controller;
 import se.kth.iv1350.model.Amount;
 import se.kth.iv1350.model.Receipt;
+import se.kth.iv1350.view.View;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -39,6 +42,20 @@ class PrinterHandlerTest {
 
         printerHandler = null;
         localDateTime = null;
+    }
+
+    private static String getItemsString(Receipt receipt) {
+        StringBuilder itemsStringBuilder = new StringBuilder();
+        for (ItemInBasketDTO item : receipt.getItemsBought()) {
+            BigDecimal amountOfItemBought = new BigDecimal(item.amountInBasket());
+            Amount totalPriceForItem = new Amount(item.price().getAmount().multiply(amountOfItemBought));
+            itemsStringBuilder.append(String.format("%-20s %10s x %-5s  %s%n",
+                    item.name(),
+                    item.price().getAmountAsStringWithCurrency(),
+                    amountOfItemBought,
+                    totalPriceForItem.getAmountAsStringWithCurrency()));
+        }
+        return itemsStringBuilder.toString();
     }
 
     @Test
@@ -79,6 +96,52 @@ class PrinterHandlerTest {
                 "------------------ End receipt -------------------" +
                 System.lineSeparator() +
                 System.lineSeparator();
+
+        String result = outContent.toString();
+        assertTrue(result.contains(receiptString), "Receipt printout for sale with no items not correct");
+    }
+
+    @Test
+    void printReceiptNoDiscount() {
+        Amount amountPayed = new Amount("500");
+        Controller controller = new Controller();
+        View view = new View(controller);
+        controller.startSale();
+        try{
+            controller.enterItemID(0);
+            controller.enterItemID(0);
+            controller.enterItemID(2);
+        }
+        catch (Exception e){
+            fail("A exception was thrown during the test \n" +
+                    "Error message: " + e.getMessage() + "\n" +
+                    "Error stack trace: " + Arrays.toString(e.getStackTrace()));
+        }
+        SaleDTO saleDTO = controller.payForSale(amountPayed);
+        Receipt receipt = new Receipt(saleDTO);
+        printerHandler.printReceipt(receipt);
+
+        String itemsString = getItemsString(receipt);
+
+        String receiptString =
+                "------------------ Begin receipt -------------------" +
+                        System.lineSeparator() +
+                        "Time of Sale: " + receipt.getDateOfSale() + " " + receipt.getTimeOfSale() +
+                        System.lineSeparator() +
+                        System.lineSeparator() +
+                        itemsString +
+                        System.lineSeparator() +
+                        String.format("%-20s %29s%n", "Total: ", receipt.getTotalPrice().getAmountAsStringWithCurrency()) +
+                        String.format("%-20s %29s%n", "VAT: ", receipt.getTotalVAT().getAmountAsStringWithCurrency()) +
+                        System.lineSeparator() +
+                        String.format("%-20s %29s%n", "Rounded Total: ",
+                                receipt.getRoundedTotalPrice().getAmountAsStringWithCurrency()) +
+                        System.lineSeparator() +
+                        String.format("%-20s %29s%n", "Cash: ", receipt.getAmountPaid().getAmountAsStringWithCurrency()) +
+                        String.format("%-20s %29s%n", "Change: ", receipt.getChange().getAmountAsStringWithCurrency()) +
+                        "------------------ End receipt -------------------" +
+                        System.lineSeparator() +
+                        System.lineSeparator();
 
         String result = outContent.toString();
         assertTrue(result.contains(receiptString), "Receipt printout for sale with no items not correct");
