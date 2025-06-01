9ed30dd09d2095bed77335923190c5f4a408c8e5
Mateusz Kaszyk <mekmmeekk@proton.me>
2025-06-01 13:12:08 +0200

    Update DieComposition.java
    Added Javadoc comments.

--- iv1350/src/se/kth/iv1350/higherGrade/DieComposition.java
+++ iv1350/src/se/kth/iv1350/higherGrade/DieComposition.java
@@ -6,15 +6,27 @@ public class DieComposition {
     private final int noSides;
     private final Random random;
 
+    /**
+     * Constructs a virtual die with the specified number of sides.
+     * @param noSides The number of sides for the dice.
+     */
     public DieComposition(int noSides) {
         this.noSides = noSides;
         random = new Random();
     }
 
+    /**
+     * Simulates a die roll.
+     * @return An <code>int</code> representing a die roll.
+     */
     public int rollDie() {
         return random.nextInt(noSides) + 1;
     }
 
+    /**
+     * Gets the number of sides on a die.
+     * @return The number of sides on the die.
+     */
     public int getSides() {
         return noSides;
     }
